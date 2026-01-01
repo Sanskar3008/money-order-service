99554bf5b6d22d3be8ad3e24739a045196e5653d
Sanskar30~ <sanskarghule230@gmail.com>
2026-01-02 00:31:45 +0530

    First Commit

---
+++ src/main/java/com/rmo/money_order_service/MoneyOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rmo.money_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmo/money_order_service/MoneyOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmo.money_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
